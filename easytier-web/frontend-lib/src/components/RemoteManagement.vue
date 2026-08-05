9e1abe937f5094aab6324b67e49f0e3e39658c59
diye <35769340+diyews@users.noreply.github.com>
2026-08-05 10:49:01 +0800

    Update RemoteManagement.vue

--- easytier-web/frontend-lib/src/components/RemoteManagement.vue
+++ easytier-web/frontend-lib/src/components/RemoteManagement.vue
@@ -581,7 +581,7 @@ onUnmounted(() => {
                 </div>
 
                 <Status v-if="(curNetworkInfo?.error_msg ?? '') === ''" v-bind:cur-network-inst="curNetworkInfo"
-                    v-bind:current-network-config="curNetworkInfo.instance_id === currentNetworkConfig.instance_id ? currentNetworkConfig : null"
+                    v-bind:current-network-config="curNetworkInfo?.instance_id === currentNetworkConfig?.instance_id ? currentNetworkConfig : null"
                     class="mb-4">
                 </Status>
                 <Message v-else severity="error" class="mb-4">{{ curNetworkInfo?.error_msg }}</Message>
